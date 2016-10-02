3a8c1791e0cbaf9cca124cd3c1becab85f608fd6
Kyle Zawacki <z95sk8@outlook.com>
2016-10-02 18:39:15 -0500

    Initial Commit

---
+++ Ready Badger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ready Badger
+//
+//  Created by Kyle Zawacki on 10/2/16.
+//  Copyright © 2016 App Factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
