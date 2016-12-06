311df535ceff18cbec1d97121e6417980b981d9b
Kyle Zawacki <z95sk8@outlook.com>
2016-12-05 18:18:13 -0600

    Fixed color issues

---
+++ Ready Badger/CustomNavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationController.swift
+//  Ready Badger
+//
+//  Created by Kyle Zawacki on 12/5/16.
+//  Copyright © 2016 App Factory. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+}
